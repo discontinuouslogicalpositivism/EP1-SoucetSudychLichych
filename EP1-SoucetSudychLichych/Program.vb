df3decfe64b8a4d2b414991567678157c2acfbbd
Štěpán Lepka <Lepka.Stepan@stud.vassboskovice.cz>
2024-06-12 10:41:24 +0200

    6.

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,37 +1,41 @@
 Imports System
 
 Module Module1
-
     Sub Main()
-
         Dim pocetCisel As Integer
 
-        Console.WriteLine("Zadejte poèet èísel:")
-
-        pocetCisel = Console.ReadLine()
+        Do
+            Console.WriteLine("Zadejte poèet èísel:")
+            Dim vstup As String = Console.ReadLine()
+            If Integer.TryParse(vstup, pocetCisel) AndAlso pocetCisel > 0 Then
+                Exit Do
+            Else
+                Console.WriteLine("Neplatný vstup. Zadejte prosím celé kladné èíslo.")
+            End If
+        Loop
 
         Dim cisla(pocetCisel - 1) As Integer
-
         For i As Integer = 0 To pocetCisel - 1
+            Dim cislo As Integer
 
-            Console.WriteLine("Zadejte èíslo " & (i + 1) & ":")
-
-            cisla(i) = Console.ReadLine()
-
+            Do
+                Console.WriteLine("Zadejte èíslo " & (i + 1) & ":")
+                Dim vstup As String = Console.ReadLine()
+                If Integer.TryParse(vstup, cislo) Then
+                    cisla(i) = cislo
+                    Exit Do
+                Else
+                    Console.WriteLine("Neplatný vstup. Zadejte prosím celé èíslo.")
+                End If
+            Loop
         Next
 
         Console.WriteLine("Zadaná èísla jsou:")
-
         For i As Integer = 0 To pocetCisel - 1
-
             Console.WriteLine(cisla(i))
-
         Next
 
         Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení.")
-
         Console.ReadKey()
-
     End Sub
-
 End Module
