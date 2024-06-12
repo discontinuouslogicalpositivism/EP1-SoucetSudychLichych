e467a928b63f2bd7f0a280a3d231558156581164
Štěpán Lepka <Lepka.Stepan@stud.vassboskovice.cz>
2024-06-12 10:18:14 +0200

    PRVNI UKOOOOOOOOL

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,7 +1,37 @@
 Imports System
 
-Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+Module Module1
+
+    Sub Main()
+
+        Dim pocetCisel As Integer
+
+        Console.WriteLine("Zadejte poèet èísel:")
+
+        pocetCisel = Console.ReadLine()
+
+        Dim cisla(pocetCisel - 1) As Integer
+
+        For i As Integer = 0 To pocetCisel - 1
+
+            Console.WriteLine("Zadejte èíslo " & (i + 1) & ":")
+
+            cisla(i) = Console.ReadLine()
+
+        Next
+
+        Console.WriteLine("Zadaná èísla jsou:")
+
+        For i As Integer = 0 To pocetCisel - 1
+
+            Console.WriteLine(cisla(i))
+
+        Next
+
+        Console.WriteLine("Stisknìte libovolnou klávesu pro ukonèení.")
+
+        Console.ReadKey()
+
+    End Sub
+
 End Module
